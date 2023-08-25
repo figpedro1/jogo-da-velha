2f96c21be6c8c13794a3ec41d3530802df10a774
Pedro Figueiredo <pedrofernando2003@gmail.com>
2023-08-25 00:28:28 -0400

    bug fix botao reiniciar

--- js/animacoes.js
+++ js/animacoes.js
@@ -5,43 +5,46 @@ const casa = [
 ];
 const reset = document.querySelector('#reiniciar');
 reset.addEventListener('mouseover', () => {
-    reset.style.backgroundColor = "#d1385e"
-})
+    reset.style.backgroundColor = "#d1385e";
+});
 
 reset.addEventListener('mouseout', () => {
     reset.style.backgroundColor = "#e83f68";
+    reset.style.height = "70px";
+    reset.style.width = "170px";
+    reset.style.fontSize = "20px";
 })
 
 reset.addEventListener('mouseup', () => {
     reset.style.backgroundColor = "#d1385e";
-    reset.style.height = "70px"
-    reset.style.width = "170px"
-    reset.style.fontSize = "20px"
-})
+    reset.style.height = "70px";
+    reset.style.width = "170px";
+    reset.style.fontSize = "20px";
+});
 
 reset.addEventListener('mousedown', () => {
     reset.style.backgroundColor = "#c1284e";
-    reset.style.height = "60px"
-    reset.style.width = "150px"
-    reset.style.fontSize = "18px"
-})
+    reset.style.height = "60px";
+    reset.style.width = "150px";
+    reset.style.fontSize = "18px";
+});
 
 for (let linha = 0; linha < casa.length; linha++){
     for (let coluna = 0; coluna < casa[linha].length; coluna++){
-        casa[linha][coluna].addEventListener('mouseover', () => {hoverCasa(linha, coluna)})
-    }
+        casa[linha][coluna].addEventListener('mouseover', () => {hoverCasa(linha, coluna)});
+    };
 };
 
 for (let linha = 0; linha < casa.length; linha++){
     for (let coluna = 0; coluna < casa[linha].length; coluna++){
-        casa[linha][coluna].addEventListener('mouseout', () => {mouseoutCasa(linha, coluna)})
-    }
+        casa[linha][coluna].addEventListener('mouseout', () => {mouseoutCasa(linha, coluna)});
+    };
 };
 
 function hoverCasa(linha, coluna){
-    casa[linha][coluna].style.backgroundColor = "#88888833"
-}
+    casa[linha][coluna].style.backgroundColor = "#88888833";
+};
 
 function mouseoutCasa(linha, coluna){
-    casa[linha][coluna].style.backgroundColor = ""
-}
+    casa[linha][coluna].style.backgroundColor = "";
+};
--- js/script.js
+++ js/script.js
@@ -20,29 +20,29 @@ function vitoria() {
     if (posicaoStatus[1][1] != 0 && ((posicaoStatus[0][0] == posicaoStatus[1][1] && posicaoStatus[1][1] == posicaoStatus[2][2]) || (posicaoStatus[0][2] == posicaoStatus[1][1] && posicaoStatus[1][1] == posicaoStatus[2][0]))) {
         finalizado = true;
         return posicaoStatus[1][1];
-    }
+    };
     for (let i = 0; i < posicaoStatus.length; i++) {
         if (posicaoStatus[i][i] != 0 && ((posicaoStatus[i][0] == posicaoStatus[i][1] && posicaoStatus[i][1] == posicaoStatus[i][2]) || (posicaoStatus[0][i] == posicaoStatus[1][i] && posicaoStatus[1][i] == posicaoStatus[2][i]))) {
             finalizado = true;
             return posicaoStatus[i][i];
-        }
-    }
+        };
+    };
     if(turno >= 9){
         finalizado = true;
         return -1;
-    }
+    };
     return 0;
 };
 
 function jogada(linha, coluna){
     if(!finalizado){
-        const vezDe = (turno % 2) + 1
+        const vezDe = (turno % 2) + 1;
         
         if(posicaoStatus[linha][coluna] == 0){
             posicaoStatus[linha][coluna] = vezDe;
-            posicao[linha][coluna].innerHTML = vezDe == 1 ? '<img class="xo" src="img/x.png">' : '<img class="xo" src="img/circulo.png">'
-            turno++
-            vencedor = vitoria()
+            posicao[linha][coluna].innerHTML = vezDe == 1 ? '<img class="xo" src="img/x.png">' : '<img class="xo" src="img/circulo.png">';
+            turno++;
+            vencedor = vitoria();
             info.innerHTML = finalizado ? (vencedor == -1 ? "Deu velha!" : (vencedor == 1 ? '<img class="vezDe" src="img/x.png">' : '<img class="vezDe" src="img/circulo.png">') + " venceu!") : "Vez de: " + (vezDe == 2 ? '<img class="vezDe" src="img/x.png">' : '<img class="vezDe" src="img/circulo.png">');
             subInfo.innerHTML = finalizado ? "Clique em reiniciar para jogar novamente!" : "";
         } else {
@@ -53,19 +53,19 @@ function jogada(linha, coluna){
 
 for (let linha = 0; linha < posicao.length; linha++){
     for (let coluna = 0; coluna < posicao[linha].length; coluna++){
-        posicao[linha][coluna].addEventListener('click', () => {jogada(linha, coluna)})
-    }
+        posicao[linha][coluna].addEventListener('click', () => {jogada(linha, coluna)});
+    };
 };
 
 reiniciar.addEventListener('click', () => {
-    info.innerHTML = 'Vez de: <img class="vezDe" src="img/x.png">'
-    subInfo.innerHTML = ""
+    info.innerHTML = 'Vez de: <img class="vezDe" src="img/x.png">';
+    subInfo.innerHTML = "";
     turno = 0;
     finalizado = false;
     for (let linha = 0; linha < posicaoStatus.length; linha++){
         for (let coluna = 0; coluna < posicaoStatus[linha].length; coluna++){
-            posicaoStatus[linha][coluna] = 0
-            posicao[linha][coluna].innerHTML = ""
-        }
-    }
+            posicaoStatus[linha][coluna] = 0;
+            posicao[linha][coluna].innerHTML = "";
+        };
+    };
 });
